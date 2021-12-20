a60bc4808f2a7763986e2a91a57e4b8b8198b218
xisen.he <xisen.he@thoughtworks.com>
2021-12-20 16:48:33 +0800

    test: add the Ceil component emit test code

--- tests/unit/Cell.spec.js
+++ tests/unit/Cell.spec.js
@@ -1,10 +1,14 @@
 import { shallowMount } from "@vue/test-utils";
 import Cell from "@/components/Cell.vue";
+import { Snake } from "@/common/snake";
 
-const factory = (type) => {
+const id = "4-4"
+const factory = () => {
+    let snake = new Snake(30, 30);
     const wrapper = shallowMount(Cell, {
         propsData: {
-            type
+            id,
+            snake
         }
     })
 
@@ -13,14 +17,31 @@ const factory = (type) => {
 
 
 describe("View Module The Cell Component", () => {
-    it("test Cell component type prop.", async () => {
-        const wrapper = factory("head");
-        expect(wrapper.find(".head").exists()).toBe(true);
-    })
-
-    it("test Cell component type prop. Default is blank", async () => {
+    it("test Cell component on emit change", async () => {
         const wrapper = factory();
-        expect(wrapper.find(".blank").exists()).toBe(true);
-    })
+        wrapper.vm.snake.emit("update", [{
+            id,
+            x: 4,
+            y: 4,
+            type: "head"
+        }])
 
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.find(".head").exists()).toBe(true);
+    });
+
+    it("test Cell component on emit change 2", async () => {
+        const wrapper = factory();
+        wrapper.vm.snake.emit("update", [{
+            id,
+            x: 4,
+            y: 4,
+            type: "body"
+        }])
+
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.find(".body").exists()).toBe(true);
+    });
 })
